d213d540b6d1d6f5ede9f41aed6fb8a6e51a5bce
xearts <xearts@user-no-MacBook-Pro.local>
2019-02-01 12:39:14 +0900

    First commit

---
+++ src/MatrixPrinterInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xearts
+ * Date: 2019-02-01
+ * Time: 12:26
+ */
+
+namespace Xeanton\ArrayRotation;
+
+
+interface MatrixPrinterInterface
+{
+
+}
---
+++ src/SimpleMatrixPrinter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xearts
+ * Date: 2019-02-01
+ * Time: 12:29
+ */
+
+namespace Xeanton\ArrayRotation;
+
+
+class SimpleMatrixPrinter
+{
+
+}
